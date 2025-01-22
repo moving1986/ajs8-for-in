ebab796940c41e12d1008cd082c8bf8fe0741ead
Alexey Mezentsev <aleksei_mezencev@mail.ru>
2025-01-22 18:33:55 +0700

    task complite

---
+++ src/js/__tests__/forin.test.js
@@ -0,0 +1,13 @@
+import  orderByProps  from '../forin';
+
+test('sort test', () => {
+  const obj = {name: 'мечник', health: 10, level: 2, attack: 80, defence: 40};
+   expect(orderByProps(obj, ["name", "level"])).toEqual([
+      {key: "name", value: "мечник"},
+      {key: "level", value: 2},
+      {key: "attack", value: 80},
+      {key: "defence", value: 40},
+      {key: "health", value: 10}
+    ]);
+
+});
---
+++ src/js/forin.js
@@ -0,0 +1,13 @@
+export default  orderByProps = (obj, order = ['']) => {
+    const keys = [
+    ...order,
+    ...Object.keys(obj)
+        .filter(key => !order.includes(key))
+        .sort()
+    ];    
+
+    return keys.map( key => ({
+        key,
+        value: obj[key]
+    }));
+}
